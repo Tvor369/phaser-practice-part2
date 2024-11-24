342840cce3272f81895f3ab81f3e75c53ccfd455
Trevor Stewart <146051077+Tvor369@users.noreply.github.com>
2024-11-24 02:48:25 +0000

    when adding a scene to project, must update the main.js file to include it

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { Game } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
+import { Settings } from './scenes/Settings';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -21,7 +22,8 @@ const config = {
         Preloader,
         MainMenu,
         Game,
-        GameOver
+        GameOver,
+        Settings
     ]
 };
 
--- src/scenes/Settings.js
+++ src/scenes/Settings.js
@@ -13,37 +13,42 @@ export class Settings extends Scene
         this.load.setPath('assets');
 
         this.load.image('bg', 'background.png');
-    }
 
-    init ()
-    {
-        //  We loaded this image in our Boot Scene, so we can continue to display it here
         this.add.image(512, 384, 'bg');
     }
 
     create ()
     {
-        this.add.text(500, 500, 'Settings', {
+        this.add.text(500, 100, 'Settings', {// Title of the settings menu
             fontFamily: 'Inknut Antiqua', fontSize: 60, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
 
-        const closeOut = this.add.text(50, 50, 'X', {// X button to close the settings menu
+        const closeOut = this.add.text(50, 50, 'X', {// temp X button to close the settings menu
             fontFamily: 'Inknut Antiqua', fontSize: 40, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'right'
         }).setInteractive().setOrigin(0.5);
 
-        closeOut.on('pointerdown', () => {
+    //  Add a hover effect to the closeOut button
+        closeOut.on('pointerover', () => {
+            closeOut.setColor('#ff0');
+        })
+        closeOut.on('pointerout', () => {
+            closeOut.setColor('#fff');
+        })
+        closeOut.on('pointerdown', () => {//Return to main menu
             this.scene.start('MainMenu');
         });
         
 
-        this.input.once('pointerdown', () => {
+        
+    }
 
-        this.scene.start('Game');
-
-        });
+    update ()
+    {
+        //  Update logic here
+        
     }
 }
